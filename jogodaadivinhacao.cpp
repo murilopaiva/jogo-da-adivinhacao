dc030fbe1d935bc615bc30f7b85450512a490801
murilo <murilopaiva22@gmail.com>
2021-02-17 04:30:27 -0300

    #02 - Adiciona verificação das tentativas

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -6,6 +6,33 @@ int main (){
     cout << "* Bem-vindos ao jogo da adivinhacao! *" << endl;
     cout << "**************************************" << endl;
 
-    int numero_secreto = 42;
-    cout << "O numero secreto eh "<< numero_secreto << ". Nao conte para ninguem!" << endl;
+    const int NUMERO_SECRETO = 42;
+
+    bool nao_acertou = true;
+    int tentativas = 0;
+
+    while(nao_acertou){
+        tentativas++;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
+        cout << "Qual seu chute? ";
+        cin >> chute;
+        cout << "O valor do seu chute eh: " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if(acertou){
+            cout << "Parabens! Voce acertou o numero secreto!" << endl;
+            nao_acertou = false;
+        }
+        else if(maior){
+            cout << "Seu chute foi maior que o numero secreto!" << endl;
+        }
+        else {
+            cout << "Seu chute foi menor que o numero secreto!" << endl;
+        }
+    }
+    cout << "Fim de jogo!" << endl;
+    cout << "Voce acertou o numero secreto em " << tentativas << " tentativas" << endl;
 }
